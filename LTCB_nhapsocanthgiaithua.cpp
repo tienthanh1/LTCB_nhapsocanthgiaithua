db0abf463ce452f19559e769c72c855688ea17ca
tien thanh <tienthanh@gmail.com>
2024-03-04 10:15:11 +0700

    Add project files.

---
+++ LTCB_nhapsocanthgiaithua.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int i = 1, sonhap, giaithua = 1;
+	cout << "nhap so can tinh giai thua:";
+	cin >> sonhap;
+	while (i <= sonhap) {
+		giaithua = giaithua * i;
+		i++; // i =i+1;
+	}
+	cout << "Giai thua cua " << sonhap << "la:" << giaithua << endl;
+	return 0;
+}
